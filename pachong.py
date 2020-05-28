423aea162bb0c596c7880f0b4b9bf161cec21a49
yumibiao <32478432+yumibiao@users.noreply.github.com>
2020-05-28 09:28:01 +0800

    Update pachong.py

--- pachong.py
+++ pachong.py
@@ -23,29 +23,38 @@ print(data2)
 #loads:把json转换为dict
 N =10000
 for i in range(1,N):
-  #url='https://v1.hitokoto.cn/?c=d&encode=json' #需要爬数据的网址
-  page=requests.Session().get(url) 
-  #tree=html.fromstring(page.text) 
-  s1 = json.loads(page.text)
-  
-  #打印statusCode对应的值
-  ss= str(s1['hitokoto'] )
-  s2= str(s1['from'])
-  ss=ss.strip().replace('\n', '').replace('\t', '').replace('\r', '').strip()
-  s2=s2.strip().replace('\n', '').replace('\t', '').replace('\r', '').strip()
-  #ss.strip().replace('\r', 'xxxabc')
-  #ss.replace('\n', '').replace(’\r’, ‘’)
-  #s2.replace('\r', '')
-  print(i,"/",N," ",ss)
-  #rstrip(ss)
-  #rstrip(s2)
-
-  tmp = ss + "『" + s2 +"』"
-  print(tmp,file=data)
-  #print("--[",file=data)
-  #print(s1['creator'],file=data)
-  #print("]",file=data)
-  #print("\n",file=data)
+  try:
+    #url='https://v1.hitokoto.cn/?c=d&encode=json' #需要爬数据的网址
+    page=requests.Session().get(url) 
+    #tree=html.fromstring(page.text) 
+    s1 = json.loads(page.text)
+    
+    #打印statusCode对应的值
+    ss= str(s1['hitokoto'] )
+    s2= str(s1['from'])
+    s3= str(s1['from_who'])
+    s3=s3.strip().replace('\n', '').replace('\t', '').replace('\r', '').strip()
+    ss=ss.strip().replace('\n', '').replace('\t', '').replace('\r', '').strip()
+    s2=s2.strip().replace('\n', '').replace('\t', '').replace('\r', '').strip()
+    #ss.strip().replace('\r', 'xxxabc')
+    #ss.replace('\n', '').replace(’\r’, ‘’)
+    #s2.replace('\r', '')
+    
+    #rstrip(ss)
+    #rstrip(s2)
+    if (s3 == 'None'):
+      tmp = ss + "『" + s2  +"』"
+    else:
+      tmp = ss + "『" + s2 + "-" +s3 +"』"
+    
+    print(i,"/",N," ",tmp)
+    print(tmp,file=data)
+    #print("--[",file=data)
+    #print(s1['creator'],file=data)
+    #print("]",file=data)
+    #print("\n",file=data)
+  except:
+    print("error!")
 
 #sss="1 1 1\r\n"
 #sss=sss.replace(' ', '').replace('\n', '').replace('\t', '').replace('\r', '').strip()
